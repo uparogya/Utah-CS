ff85f96ae6dfcc19a4554eb33aabf3c091178ace
haihan-lin <haihan.lin.7@gmail.com>
2022-08-02 11:37:32 -0600

    add tooltip and dialog for course list

--- src/Components/Toolbox.tsx
+++ src/Components/Toolbox.tsx
@@ -1,12 +1,19 @@
-import { Stack, Chip, Box } from "@mui/material";
+import { Stack, Chip, Container, Box, Dialog, DialogTitle, List, ListItem, IconButton, Tooltip, Button, DialogActions } from "@mui/material";
 import { observer } from "mobx-react-lite";
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import Store from "../Interface/Store";
 import { PossibleCategories } from "../Preset/Constants";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { CategoryContext } from "../App";
 
 const Toolbox: FC = () => {
 
     const store = useContext(Store);
+
+    const categories = useContext(CategoryContext);
+    console.log(categories);
+
+    const [openCategoryDialog, setDialog] = useState('');
     const chipClickHandler = (chipName: string) => {
         if (store.selectedCategory.includes(chipName)) {
             store.setCategory(store.selectedCategory.filter(d => d !== chipName));
@@ -18,13 +25,31 @@ const Toolbox: FC = () => {
         <Box>
             <Stack>
                 {PossibleCategories.map((chipName) => (
-                    <Chip label={chipName}
-                        clickable
-                        onClick={() => chipClickHandler(chipName)}
-                        style={{ margin: '5px' }}
-                        color={store.selectedCategory.includes(chipName) ? 'primary' : 'default'} />
+                    <Container key={`${chipName}-chip`}>
+
+                        <Chip label={chipName}
+                            clickable
+                            onClick={() => chipClickHandler(chipName)}
+                            style={{ margin: '5px' }}
+                            color={store.selectedCategory.includes(chipName) ? 'primary' : 'default'} />
+                        <Tooltip title='See list of courses'>
+                            <IconButton onClick={() => setDialog(chipName)}>
+                                <InfoOutlinedIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Container>
                 ))}
             </Stack>
+            <Dialog open={Boolean(openCategoryDialog)} onClose={() => setDialog('')}>
+                <DialogTitle>{openCategoryDialog} courses</DialogTitle>
+                <List>
+                    {categories.map((cate) => cate['category'] === openCategoryDialog ? <ListItem key={cate['core_code']}>{cate['core_short_desc']}</ListItem> : <></>
+                    )}
+                </List>
+                <DialogActions>
+                    <Button onClick={() => setDialog('')}>close</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 };
