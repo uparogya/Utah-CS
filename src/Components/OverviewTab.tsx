59985a4b89495cb7f0a676e22e463e73957ce54b
haihan-lin <haihan.lin.7@gmail.com>
2023-04-28 16:44:02 -0600

    change class to course

--- src/Components/OverviewTab.tsx
+++ src/Components/OverviewTab.tsx
@@ -91,7 +91,7 @@ const OverviewTab: FC = () => {
                 <Typography style={{ textAlign: 'left' }} color='#3d3d3d'>
                     <ul>
                         <li>
-                            <b>Core CS Courses</b> - these classes directly teach fundamental computer science or programming skills. They are divided into two categories:
+                            <b>Core CS Courses</b> - these courses directly teach fundamental computer science or programming skills. They are divided into two categories:
                             <ul>
                                 <li>
                                     <b>Basic CS Courses</b> - introductory CS Courses for those with no prior experience in the area
@@ -102,7 +102,7 @@ const OverviewTab: FC = () => {
                             </ul>
                         </li>
                         <li>
-                            <b>Related CS Courses</b> - the emphasis of these classes is on the application, rather than the skills, of computer science in a variety of settings.
+                            <b>Related CS Courses</b> - the emphasis of these courses is on the application, rather than the skills, of computer science in a variety of settings.
 
                         </li>
                     </ul>
